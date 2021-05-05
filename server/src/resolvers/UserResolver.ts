3e2b8ac52057890ec84feb58c82b1d246c1f2eeb
Gokul2406 <gokulpbharathan89@gmail.com>
2021-05-05 20:13:30 +0530

    ✌  Added login

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,4 +1,4 @@
-import {Resolver, Query, Mutation, InputType, Field,  Arg} from "type-graphql";
+import {Resolver, Query, Mutation, InputType, Field, Arg, ObjectType} from "type-graphql";
 import User from "../entity/User"
 import argon2 from "argon2"
 
@@ -10,6 +10,26 @@ class UserInput {
     @Field()
     password: string;
 }
+
+@ObjectType()
+class Error {
+    @Field()
+    field: string;
+
+    @Field()
+    message: string;
+}
+
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [Error], {nullable: true})
+    errors?: Error[];
+
+    @Field(() => User, {nullable: true})
+    user?: User
+}
+
 
 @Resolver()
 export default class UserResolver {
@@ -23,7 +43,38 @@ export default class UserResolver {
         @Arg('options') options: UserInput
     ): Promise<User> {
        const hashedPassword = await argon2.hash(options.password);
-       const user = User.create({username: options.username, password: hashedPassword}).save();
+       const user = User.create({username: options.username, password: hashedPassword}).save(); 
        return user
     }
-}
+    @Mutation(() => UserResponse)
+    async login(
+    @Arg('options') options: UserInput
+    ): Promise<UserResponse> {
+        const user = await User.findOne({username: options.username})
+        if (!user) {
+            return {
+                errors: [{
+                    field: "username",
+                    message: "No user with that username"
+                }]           
+            }
+        }
+        const valid = await argon2.verify(user.password, options.password)
+        if (!valid) {
+            return {
+                errors: [
+        {
+            field: "password",
+            message: "password is incorrect"
+        }
+                ]
+            }
+        }
+        return {
+            user
+        }
+        
+    }
+    }
+
+
